afcd0df97dbcc72b25d30f90d9e074501b69144a
Gabriel Santos Cardoso <eng.gabrielscardoso@gmail.com>
2021-12-23 16:04:11 -0300

    feat: finish calculator model

--- src/components/Calculator.svelte
+++ src/components/Calculator.svelte
@@ -6,37 +6,41 @@
   import Row from "./Row.svelte";
 
   let calc: any = new Calculator();
-  let displayed: any = n => calc = calc.displayedValue(n);
+  let accumulatorClear: any = _ => calc = calc.accumulatorClear();
+  let displayed: any = (n: string) => calc = calc.displayedValue(n);
+  let displayedDot: any = _ => calc = calc.displayedDot();
+  let operation: any = (op: string) => calc = calc.operation(op);
+  let performOperation: any = _ => calc = calc.performOperation();
 </script>
 
 <div class="calculator">
   <Display value={calc.value}></Display>
   <Row>
-    <Button value="AC" triple emphasis></Button>
-    <Button value="/" operation></Button>
+    <Button value="AC" triple emphasis getValues={accumulatorClear}></Button>
+    <Button value="/" operation getValues={operation}></Button>
   </Row>
   <Row>
     <Button value="7" operator getValues={displayed}></Button>
     <Button value="8" operator getValues={displayed}></Button>
     <Button value="9" operator getValues={displayed}></Button>
-    <Button value="*" operation></Button>
+    <Button value="*" operation getValues={operation}></Button>
   </Row>
   <Row>
-    <Button value="6" operator getValues={displayed}></Button>
-    <Button value="5" operator getValues={displayed}></Button>
     <Button value="4" operator getValues={displayed}></Button>
-    <Button value="+" operation></Button>
+    <Button value="5" operator getValues={displayed}></Button>
+    <Button value="6" operator getValues={displayed}></Button>
+    <Button value="+" operation getValues={operation}></Button>
   </Row>
   <Row>
-    <Button value="3" operator getValues={displayed}></Button>
-    <Button value="2" operator getValues={displayed}></Button>
     <Button value="1" operator getValues={displayed}></Button>
-    <Button value="-" operation></Button>
+    <Button value="2" operator getValues={displayed}></Button>
+    <Button value="3" operator getValues={displayed}></Button>
+    <Button value="-" operation getValues={operation}></Button>
   </Row>
   <Row>
     <Button value="0" double operator getValues={displayed}></Button>
-    <Button value="" operator getValues={displayed}></Button>
-    <Button value="=" emphasis></Button>
+    <Button value="," operator getValues={displayedDot}></Button>
+    <Button value="=" emphasis getValues={performOperation}></Button>
   </Row>
 </div>
 
--- src/models/Calculator.ts
+++ src/models/Calculator.ts
@@ -1,3 +1,4 @@
+const CLEAR_SCREEN: boolean = true;
 const NOT_CLEAR_SCREEN: boolean  = false;
 
 export default class Calculator {
@@ -19,15 +20,49 @@ export default class Calculator {
   }
 
   get value(): string{
-    return this.#value?.replace('.', ',') || '0';
+    return this.#value?.replace(',', '.') || '0';
   }
-
-  displayedValue(newValue: string) {
+  
+  displayedValue(newValue: string): Calculator {
     return new Calculator(
       (this.#clearScreen || !this.#value) ? newValue : this.#value + newValue, 
       this.#accumulator, 
       NOT_CLEAR_SCREEN, 
       this.#operation
-    )
-  }
+      );
+    }
+    
+    displayedDot(): Calculator {
+      return new Calculator(
+        this.#value?.includes('.') ? this.#value : this.#value + '.', 
+        this.#accumulator, 
+        NOT_CLEAR_SCREEN, 
+        this.#operation
+        );
+      }
+    
+    accumulatorClear(): Calculator{
+      return new Calculator();
+    }
+
+    operation(nextOperation: string) {
+      return this.performOperation(nextOperation);
+    }
+
+    performOperation(nextOperation: string = null) {
+      const accumulator: any = !this.#operation 
+        ? parseFloat(this.#value)
+        : eval(`${this.#accumulator} ${this.#operation} ${this.#value}`);
+
+      const value: string = !this.operation 
+        ? this.#value
+        : `${accumulator}`;
+
+        return new Calculator(
+          value, 
+          accumulator,
+          nextOperation ? CLEAR_SCREEN : NOT_CLEAR_SCREEN,
+          nextOperation
+        )
+    }
 }
